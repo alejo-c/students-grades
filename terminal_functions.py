657d6a4427da846fc6c01bbee2cb83394eb2829d
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-01-16 15:54:41 -0500

    Add a optional msg parameter in the pause function

--- terminal_functions.py
+++ terminal_functions.py
@@ -5,5 +5,5 @@ def clear() -> None:
     os.system('clear' if platform.system() == "Linux" else 'cls')
 
 
-def pause() -> None:
-    input('\n...Press <ENTER> to continue...\n')
+def pause(message: str = '') -> None:
+    input(f'{message}\n...Press <ENTER> to continue...\n')
